2f92d3cdd05a9e3fb883f43baf18b577022c5b71
sahil katiyar <sahilkatiyar2024@gmail.com>
2025-01-01 08:06:01 +0530

    Time: 970 ms (21.79%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -2,3 +2,6 @@
 select customer_id
 from Customer group by customer_id
 having count(distinct product_key)=(select count(*) from Product)
+#here I did a mistake of writing a number ex-
+#count(product_key)=2
+#I wrote count is equal to 2 because here product table has only 2 values but it might be different for different test cases.
